224ec91f995e095e545903b9d41b4e450b14ed97
Hugues Tchokouani <Hugodubled@outlook.com>
2022-02-14 16:39:39 -0500

    adding files

--- JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -22,19 +22,17 @@ public class JavaApplication17 {
         listValues[3] = 5;
         listValues[4] = 35;
         listValues[5] = 14;
-        int i=0, pos=0, D= listValues[0];
-        
+        int i = 0, pos = 0, Max = listValues[0];
+
         while (i < listValues.length) {
-            if ( D >= listValues[i]) {
-                D = D;
-            } else{
-                D = listValues[i];
-                pos =i;
+            if (Max <= listValues[i]) {
+                Max = listValues[i];
+                pos = i;
             }
             i++;
         }
-         System.out.println(+D);
-         System.out.println(+pos);    
+        System.out.println("La valeur maximale est: " + Max + " et occupe la position: " + pos);
+        System.out.println();
     }
 
 }
